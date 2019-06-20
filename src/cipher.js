e8810adfa8af07914d56d813b54db22723692eec
rosario VS <paradisekiss-yaoi@hotmail.com>
2019-06-20 10:45:08 -0500

    ultima subida

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,34 @@
 window.cipher = {
-  encode: () => {
-
+  encode: (offset, str) => {
+    let codeCifrado = 0;
+    let textCifrado = '';
+    for (let i = 0; i < str.length; i++){
+    const ascii = str.charCodeAt(i);
+    if (ascii >= 65 && ascii <= 90){
+      codeCifrado = (ascii - 65 + offset) % 26 + 65;
+    } else if (ascii >= 97 && ascii <= 122){
+      codeCifrado = (ascii -97 + offset) % 26 +97;
+    } else {
+      codeCifrado = ascii;
+    }
+    textCifrado += String.fromCharCode(codeCifrado);
   }
+  return textCifrado;
+  }, 
+   decode: (offset2, str) => {
+     let codeDescifrado = 0;
+     let textDescifrado = '';
+     for (let i = 0; i < str.length; i++) {
+       const ascii = str.charCodeAt(i);
+     if (ascii >= 65 && ascii <= 90) {
+       codeDescifrado = (ascii -90 -offset2)% 26 +90;
+     } else if (ascii >= 97 && ascii <= 122){
+       codeDescifrado = (ascii -122 -offset2) % 26 +122;
+     }else {
+       codeDescifrado = ascii;
+     }
+     textDescifrado += String.fromCharCode(codeDescifrado);
+     }
+     return textDescifrado;
+   }
 };
--- src/index.js
+++ src/index.js
@@ -15,3 +15,49 @@ const pagDCipher = () => {
 
 const lass2 = document.getElementById ("lass2");
 lass2.addEventListener ("click", pagDCipher);
+
+const showInfo = () => {
+    let apareceInfo = document.getElementById ("instrucciones");
+    apareceInfo.style.display = "block";
+    return apareceInfo;
+}
+
+const info = document.getElementById ("Info");
+info.addEventListener ("click", showInfo);
+
+const closeInfo= () => {
+    let cierraInfo = document.getElementById ("instrucciones");
+    cierraInfo.style.display = "none";
+    return cierraInfo;
+}
+
+const cerrarVen = document.getElementById ("cerrar");
+cerrarVen.addEventListener ("click", closeInfo);
+
+const returnHome = () => {
+    location.reload ();
+}
+
+const returnPag = document.getElementById ("regini");
+returnPag.addEventListener ("click", returnHome);
+
+const returnPagTwo = document.getElementById ("reginiTwo");
+returnPagTwo.addEventListener ("click", returnHome);
+
+const ingresarTexto = document.getElementById("inputText");
+const offset = document.getElementById("nomber");
+const btnCifrar = document.getElementById("ocult");
+const desciTexto = document.getElementById("inputDes")
+
+btnCifrar.addEventListener ("click", () => {
+    desciTexto.value = window.cipher.encode (parseInt(offset.value), ingresarTexto.value)
+})
+
+const ingresarTexto2 = document.getElementById("visible");
+const offset2 = document.getElementById("nomber2");
+const btnCifrar2 = document.getElementById("discover");
+const desciTexto2 = document.getElementById("inputM")
+
+btnCifrar2.addEventListener ("click", () => {
+    desciTexto2.value = window.cipher.decode (parseInt(offset2.value), ingresarTexto2.value)
+})
